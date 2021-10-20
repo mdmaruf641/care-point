46b7027bba84e31ac88f7e5f1a6e70958066818a
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-10-20 17:07:18 +0600

    about us section added

---
+++ src/Components/Pages/Home/About/About.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./About.css";
+import aboutImg from "../../../../images/about-img.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+const About = () => {
+  const star = <FontAwesomeIcon icon={faStar} />;
+
+  return (
+    <div className="about" id="about">
+      <Container>
+        <div className="about-heading">
+          <h2 className="fw-bold">Why Choose Us ?</h2>
+          <p className="w-75 mx-auto">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </p>
+        </div>
+
+        <div className="about-items">
+          <Row>
+            <div className="col-md-4">
+              <Row className="single-items">
+                <div className="items-head d-flex justify-content-center align-items-center">
+                  <div className="icon-body">
+                    <h3 className="icon me-3 fs-4">{star}</h3>
+                  </div>
+                  <div className="title">
+                    <h4 className="fw-bolder">Professional</h4>
+                  </div>
+                </div>
+                <div className="desc">
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium.
+                  </p>
+                </div>
+              </Row>
+              <Row className="single-items">
+                <div className="items-head d-flex justify-content-center align-items-center">
+                  <div className="icon-body">
+                    <h3 className="icon me-3 fs-4">{star}</h3>
+                  </div>
+                  <div className="title ">
+                    <h4 className="fw-bolder">Professional</h4>
+                  </div>
+                </div>
+                <div className="desc">
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium.
+                  </p>
+                </div>
+              </Row>
+            </div>
+            <div className="col-md-4">
+              <img className="w-75 mx-auto" src={aboutImg} alt="" />
+            </div>
+            <div className="col-md-4">
+              <Row className="single-items">
+                <div className="items-head d-flex justify-content-center align-items-center">
+                  <div className="icon-body">
+                    <h3 className="icon me-3 fs-4">{star}</h3>
+                  </div>
+                  <div className="title">
+                    <h4 className="fw-bolder">Professional</h4>
+                  </div>
+                </div>
+                <div className="desc">
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium.
+                  </p>
+                </div>
+              </Row>
+              <Row className="single-items">
+                <div className="items-head d-flex justify-content-center align-items-center">
+                  <div className="icon-body">
+                    <h3 className="icon me-3 fs-4">{star}</h3>
+                  </div>
+                  <div className="title ">
+                    <h4 className="fw-bolder">Professional</h4>
+                  </div>
+                </div>
+                <div className="desc">
+                  <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium.
+                  </p>
+                </div>
+              </Row>
+            </div>
+          </Row>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default About;
